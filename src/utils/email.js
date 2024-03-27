0da48a6aa3dc86ab49508ea8ed41fe1d99582aa2
mraidi achref <mradiachref@gmail.com>
2024-03-27 16:28:10 +0100

    using a gmail to send mails

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,8 +2,9 @@ const nodemailer = require('nodemailer');
 
 const sendEmail =  async (option) => {
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST, 
-        port: process.env.EMAIL_USER , 
+       // host: process.env.EMAIL_HOST, 
+       // port: process.env.EMAIL_USER , 
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
@@ -11,7 +12,7 @@ const sendEmail =  async (option) => {
     })
 
     const emailOptions = {
-        from: 'Cineflix support<support@cineflix.com>',
+        from: `${process.env.EMAIL_USER}`,
         to: option.email,
         subject: option.subject,
         text: option.message
